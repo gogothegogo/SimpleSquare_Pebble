ef9ec92cf8102d8d8c8113b3790d7636976cdd32
gogothegogo <gogothegogo@users.noreply.github.com>
2016-04-27 21:46:39 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,6 +18,8 @@ function loadOptions() {
   var $colorTheme = $('#colorTheme');
   var $timeSize = $('#timeSize');
   var $dateSize = $('#dateSize');
+  var $monthString = $('#$monthString');
+  var $dayString = $('#$dayString');
   var $bluetoothAlarm = $('#bluetoothAlarm');
   var $batteryIcon = $('#batteryIcon');
 
@@ -25,6 +27,8 @@ function loadOptions() {
     $colorTheme[0].value = localStorage.colorTheme;
     $timeSize[0].value = localStorage.timeSize;
     $dateSize[0].value = localStorage.dateSize;
+    $monthString[0].value = localStorage.monthString;
+    $dayString[0].value = localStorage.dayString;
     $bluetoothAlarm[0].value = localStorage.bluetoothAlarm;
     $batteryIcon[0].value = localStorage.batteryIcon;
   }
@@ -34,6 +38,8 @@ function getAndStoreConfigData() {
   var $colorTheme = $('#colorTheme');
   var $timeSize = $('#timeSize');
   var $dateSize = $('#dateSize');
+  var $monthString = $('#$monthString');
+  var $dayString = $('#$dayString');
   var $bluetoothAlarm = $('#bluetoothAlarm');
   var $batteryIcon = $('#batteryIcon');
 
@@ -41,6 +47,8 @@ function getAndStoreConfigData() {
     colorTheme: $colorTheme.val(),
     timeSize: $timeSize.val(),
     dateSize: $dateSize.val(),
+    monthString: $monthString.val(),
+    dayString: $dayString.val(),
     bluetoothAlarm: $bluetoothAlarm.val(),
     batteryIcon: $batteryIcon.val()
   };
@@ -48,6 +56,8 @@ function getAndStoreConfigData() {
   localStorage.colorTheme = options.colorTheme;
   localStorage.timeSize = options.timeSize;
   localStorage.dateSize = options.dateSize;
+  localStorage.monthString = options.monthString;
+  localStorage.dayString = options.dayString;
   localStorage.bluetoothAlarm = options.bluetoothAlarm;
   localStorage.batteryIcon = options.batteryIcon;
 
